4bdf887c320e26aee8ce6401c7776475ef2a6957
Elena Savinykh <gudulina@mail.ru>
2022-09-18 11:56:30 +0700

    Finish

--- HW4.py
+++ HW4.py
@@ -41,35 +41,44 @@
 # get_unique_sp(numbers)
 
 
-# 4. Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+# 4. Задана натуральная степень k.
+# Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
 # Пример:
 # k=2 => 2*d² + 4*d + 5 = 0 или d² + 5 = 0 или 10*d² = 0
-from random import randint
-import itertools
-k = int(input('k = '))
 
-def get_koeff(k):
-    koeff = [randint(0, 100) for i in range (k + 1)]
-    while koeff[0] == 0:
-        koeff[0] = randint(1, 100) 
-    return koeff
+# from random import randint
+# import itertools
 
-def get_polynomial(k, koeff):
-    var = ['*x^']*(k-1) + ['*x']
-    polynomial = [[a, b, c] for a, b, c  in itertools.zip_longest(koeff, var, range(k, 1, -1), fillvalue = '') if a !=0]
-    for x in polynomial:
-        x.append(' + ')
-    polynomial = list(itertools.chain(*polynomial))
-    polynomial[-1] = ' = 0'
-    return "".join(map(str, polynomial)).replace(' 1*x',' x')
+# k = int(input('k = '))
 
-koeff = get_koeff(k)
-polynom = get_polynomial(k, koeff)
-print(polynom)
+# def get_koeff(k):
+#     koeff = [randint(0, 100) for i in range (k + 1)]
+#     while koeff[0] == 0:
+#         koeff[0] = randint(1, 100) 
+#     return koeff
 
-with open('Polynomial.txt', 'w') as data:
-    data.write(polynom)
+# def get_polynomial(k, koeff):
+#     var = ['*x^']*(k-1) + ['*x']
+#     polynomial = [[a, b, c] for a, b, c  in itertools.zip_longest(koeff, var, range(k, 1, -1), fillvalue = '') if a !=0]
+#     for x in polynomial:
+#         x.append(' + ')
+#     polynomial = list(itertools.chain(*polynomial))
+#     polynomial[-1] = ' = 0'
+#     return "".join(map(str, polynomial)).replace(' 1*x',' x')
+
+# koeff = get_koeff(k)
+# polynom = get_polynomial(k, koeff)
+# print(polynom)
+
+# with open('Polynomial.txt', 'w') as data:
+#     data.write(polynom)
 
 
+# 5. Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
 
-# Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
+from sympy.parsing.sympy_parser import parse_expr
+
+expression = "-2+x^1-3*x^2+x^2+100*x^3-2*x"
+x = 0
+formula = expression.replace('x', str(x)).replace('^', '**')
+print(parse_expr(formula))
